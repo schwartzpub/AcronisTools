b39b3b06f488f422cda033dcd6a48332164d0dcd
Jacob Schwartz <jake@schwartzpub.com>
2022-03-05 12:29:50 -0600

    commit

--- AcronisTools.psm1
+++ AcronisTools.psm1
@@ -1,1 +1,25 @@
-#placeholder
+function Get-AcronisSecretStore {
+    <#
+    .SYNOPSIS
+        Gets a Powershell Secret Store used for Acronis Tools.
+    .DESCRIPTION
+        Gets secret store used for logging into Acronis.
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Position = 0, ValueFromPipeline = $true, ValueFromRemainingArguments = $true, Mandatory = $true)]
+        [string]$Name
+    )
+
+    if(-not (Get-Module Microsoft.PowerShell.SecretManagement)){
+        Write-Error "You do not have Microsoft.PowerShell.SecretManagement (required) installed, would you like to install it now?"
+    }
+
+    if(-not (Get-Module Microsoft.PowerShell.SecretStore)){
+        Write-Error "You do not have Microsoft.PowerShell.SecretStore (required) installed, would you like to install it now?"
+    }
+
+    if (-not (Get-SecretInfo -Name $Name)){
+        Write-Error "Secret store ($($Name)) does not exist."
+    }
+}
--- Get-AcronisSecretStore.ps1
+++ Get-AcronisSecretStore.ps1
@@ -7,16 +7,19 @@ function Get-AcronisSecretStore {
     #>
     [CmdletBinding()]
     param(
-        [Parameter(Position = 0, ValueFromPipeline = $true, ValueFromRemainingArguments = $true)]
+        [Parameter(Position = 0, ValueFromPipeline = $true, ValueFromRemainingArguments = $true, Mandatory = $true)]
         [string]$Name
     )
 
-    begin {
-        if(-not (Get-Module Microsoft.PowerShell.SecretManagement)){
-            Write-Error "You do not have Microsoft.PowerShell.SecretManagement (required) installed, would you like to install it now?"
-        }
-        if(-not (Get-Module Microsoft.PowerShell.SecretStore)){
-            Write-Error "You do not have Microsoft.PowerShell.SecretStore (required) installed, would you like to install it now?"
-        }
+    if(-not (Get-Module Microsoft.PowerShell.SecretManagement)){
+        Write-Error "You do not have Microsoft.PowerShell.SecretManagement (required) installed, would you like to install it now?"
+    }
+
+    if(-not (Get-Module Microsoft.PowerShell.SecretStore)){
+        Write-Error "You do not have Microsoft.PowerShell.SecretStore (required) installed, would you like to install it now?"
+    }
+
+    if (-not (Get-SecretInfo -Name $Name)){
+        Write-Error "Secret store ($($Name)) does not exist."
     }
 }
