eb163bae4a2ce9b7fbedd923848f559b43911074
Jacob Schwartz <jake@schwartzpub.com>
2022-03-05 12:10:54 -0600

    Separate File

---
+++ Get-AcronisSecretStore.ps1
@@ -0,0 +1,22 @@
+function Get-AcronisSecretStore {
+    <#
+    .SYNOPSIS
+        Gets a Powershell Secret Store used for Acronis Tools.
+    .DESCRIPTION
+        Gets secret store used for logging into Acronis.
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Position = 0, ValueFromPipeline = $true, ValueFromRemainingArguments = $true)]
+        [string]$Name
+    )
+
+    begin {
+        if(-not (Get-Module Microsoft.PowerShell.SecretManagement)){
+            Write-Error "You do not have Microsoft.PowerShell.SecretManagement (required) installed, would you like to install it now?"
+        }
+        if(-not (Get-Module Microsoft.PowerShell.SecretStore)){
+            Write-Error "You do not have Microsoft.PowerShell.SecretStore (required) installed, would you like to install it now?"
+        }
+    }
+}
